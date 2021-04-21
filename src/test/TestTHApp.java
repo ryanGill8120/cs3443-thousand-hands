c78d31554a02e52cbc8575e33e80787cbf1cc7eb
ryanGill8120 <ryan.gill8120@gmail.com>
2021-04-20 22:23:54 -0500

    Fixed JUnit Bug, added comments

--- src/test/TestTHApp.java
+++ src/test/TestTHApp.java
@@ -2,25 +2,48 @@ package test;
 
 import static org.junit.Assert.*;
 
+
+import java.io.FileOutputStream;
 import java.io.IOException;
+
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import application.InvalidUserException;
 import model.Model;
 
 public class TestTHApp {
 	
-	String username = "abc123";
-	String item = "Berries";
-	int amount = 23;
-	int subAmount = 5;
+	public static String username = "qte234";
+	public static String item = "Plates";
+	public static int amount = 23;
+	public static int subAmount = 5;
 	
 
-	@Before
-	public void setUp() throws Exception {
+	//Set up include removing the test username (for multiple runs of the code)
+	//I tried the same code in an AfterClass method, but it did not work there.
+	@BeforeClass
+	public static void setUp() throws Exception {
 		Model.loadFiles();
+		FileOutputStream writer = new FileOutputStream(Model.file,true);
+		FileOutputStream userWriter = new FileOutputStream(Model.userFile, true);
+		Model.hash.remove(item);
+		Model.prop.remove(item);
+		Model.prop.putAll(Model.hash);
+		Model.prop.store(writer,null);  //writes to file
+		writer.close();
+		
+		Model.users.remove(username);
+		Model.userProp.remove(username);
+		Model.userProp.putAll(Model.users);
+		Model.userProp.store(userWriter,null);  //writes to file
+		writer.close();
 	}
 	
+	//addItem returns a boolean based on whether or not the item added was already in inventory
+	//so assertion compares amount to actual quantity in the hashmap
 	@Test
 	public void testAddItem() throws IOException{
 		Model.addItem(item, amount + "");
@@ -28,17 +51,24 @@ public class TestTHApp {
 		
 	}
 	
+	//simple comparison of amounts
+	@Test
 	public void testGetNumberOfItemsInInventory() throws IOException{
 		assertEquals(amount, Model.getNumberOfItemsInInventory(item));
 	}
 	
+	//passes a subtraction expression and then compares to the return (difference) of subtractItem
+	@Test
 	public void testSubtractItem() throws IOException{
 		assertEquals(amount - subAmount, Model.subtractItem(item, subAmount + ""));
 	}
 	
+	//addUser returns true if the user already exists in inventory
+	@Test
 	public void testAddUser() throws IOException, InvalidUserException{
 		assertFalse(Model.addUserName(username));
 	}
+	
 	
 
 }
